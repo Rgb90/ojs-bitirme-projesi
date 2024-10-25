ac7995bfdb93ba8d014b42aaf1e867436ee915ab
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-10-25 09:49:56 +0300

    mobil düzenleme

--- src/components/navbar/MenuNavbarMobil.tsx
+++ src/components/navbar/MenuNavbarMobil.tsx
@@ -1,74 +1,80 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { MdMenu } from "react-icons/md";
 import { HiChevronRight } from "react-icons/hi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 interface MenuDataProps {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
 }
 
 interface MenuInfoProps {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
 }
 
 const MenuDataLinks: MenuDataProps[] = [
-    { id: 1, title: "PROTEİN" },
-    { id: 2, title: "SPOR GIDALARI" },
-    { id: 3, title: "SAĞLIK" },
-    { id: 4, title: "GIDA" },
-    { id: 5, title: "VİTAMİN" },
-    { id: 6, title: "TÜM ÜRÜNLER" },
+  { id: 1, title: "PROTEİN" },
+  { id: 2, title: "SPOR GIDALARI" },
+  { id: 3, title: "SAĞLIK" },
+  { id: 4, title: "GIDA" },
+  { id: 5, title: "VİTAMİN" },
+  { id: 6, title: "TÜM ÜRÜNLER" },
 ];
 
 const MenuInfoLinks: MenuInfoProps[] = [
-    { id: 1, title: "HESABIM" },
-    { id: 2, title: "MÜŞTERİ YORUMLARI" },
-    { id: 3, title: "İLETİŞİM" },
+  { id: 1, title: "HESABIM" },
+  { id: 2, title: "MÜŞTERİ YORUMLARI" },
+  { id: 3, title: "İLETİŞİM" },
 ];
 
 const MenuNavbarMobil = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <nav >
-            <div onClick={() => setIsMenuOpen(!isMenuOpen)} className='absolute left-8 top-4 cursor-pointer md:hidden'>
-                <MdMenu />
-            </div>
-            {/* linke items */}
-            <div className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${isMenuOpen ? 'top-12' : 'top-[-490px]'}`}>
-                <ul className="flex flex-col w-full space-y-2 py-4 border-b border-gray-200 bg-white text-left">
-                    {MenuDataLinks.map((data, index) => (
-                        <li className="text-dark font-bold w-full" key={data.id}>
-                            <Link
-                                to="/"
-                                className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 w-full"
-                            >
-                                {data.title}
-                                {/* Son eleman dışında ikon göster */}
-                                {index !== MenuDataLinks.length - 1 && (
-                                    <HiChevronRight className="ml-auto" />
-                                )}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <ul className="flex flex-col w-full space-y-2 py-4 bg-gray-100 text-left">
-                    {MenuInfoLinks.map((info) => (
-                        <li className="w-full font-bold" key={info.id}>
-                            <Link
-                                to="/"
-                                className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200 text-sm w-full"
-                            >
-                                {info.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="shadow-md  top-0 left-0 z-50">
+      <div
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="absolute left-8 top-4 cursor-pointer md:hidden"
+      >
+        <MdMenu className="text-gray-700 text-2xl" />
+      </div>
+      {/* link items */}
+      <div
+        className={`md:flex md:items-center md:pb-0 absolute md:static bg-white left-0 w-[90%]  md:w-auto md:pl-0 transition-all duration-500 ease-in ${
+          isMenuOpen ? "top-12 fixed" : "top-[-600px]"
+        }`}
+      >
+        <ul className="flex flex-col space-y-2 py-4 border-b border-gray-200 bg-white text-left">
+          {MenuDataLinks.map((data, index) => (
+            <li className="text-dark font-bold w-full" key={data.id}>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 w-full"
+              >
+                {data.title}
+                {index !== MenuDataLinks.length - 1 && (
+                  <HiChevronRight className="ml-auto text-gray-500" />
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex flex-col w-full space-y-2 py-4 bg-gray-100 text-left">
+          {MenuInfoLinks.map((info) => (
+            <li className="w-full font-bold" key={info.id}>
+              <Link
+                to="/"
+                className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200 text-sm w-full"
+              >
+                {info.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default MenuNavbarMobil;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,6 @@ import { FaRegUser } from "react-icons/fa6";
 import { PiShoppingCartLight } from "react-icons/pi";
 import { FaCaretDown } from "react-icons/fa";
 import logo from "../../assets/logo-siyah.png";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import MobilCartIcon from "./MobilCartIcon";
 import MenuNavbarWeb from "./MenuNavbarWeb";
 import InfoNavbarWeb from "./InfoNavbarWeb";
@@ -12,17 +11,14 @@ import InfoNavbarMobil from "./InfoNavbarMobil";
 function Navbar() {
   return (
     <nav>
-
       <MobilCartIcon />
 
       {/* input ve buttonlar desktop için */}
       <div className="container mx-auto w-full items-center md:flex hidden p-4 justify-evenly">
         {/* Logo */}
         <Link className="" to="/">
-        
           <img className="" src={logo} alt="Brand-logo" />
         </Link>
-
         <div className="hidden md:flex flex-col md:flex-row items-center justify-center mt-4 md:mt-0 w-full space-x-6 text-sm">
           <div className="flex items-center max-w-screen md:w-4/12">
             <input
@@ -38,39 +34,29 @@ function Navbar() {
             </button>
           </div>
 
-          <Menu as="div" className="relative inline-block text-left">
-            <MenuButton className="flex justify-between items-center bg-white h-8 md:h-10 px-4 text-gray-500 border-2 border-gray-500 rounded hover:bg-gray-50 w-full lg:w-[135px] md:w-[115px]">
-              <FaRegUser className="mr-2 h-4 w-4 md:h-5 md:w-5" />
-              HESAP
-              <FaCaretDown
-                aria-hidden="true"
-                className="-mr-1 h-4 w-4 md:h-5 md:w-5"
-              />
-            </MenuButton>
-            <MenuItems
-              transition
-              className="absolute z-10 mt-2 w-36 origin-top-right rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  <button
-                    type="submit"
-                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Çıkış yap
-                  </button>
-                </MenuItem>
-                <MenuItem>
-                  <button
-                    type="submit"
-                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Üye ol
-                  </button>
-                </MenuItem>
-              </div>
-            </MenuItems>
-          </Menu>
+
+          <button id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+          </svg>
+          </button>
+
+          <div id="dropdownDelay" className="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDelayButton">
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+              </li>
+            </ul>
+          </div>
+
 
           <Link to="/cart" className="flex-shrink-0">
             <button className="bg-gray-500 h-8 md:h-10 text-white border rounded flex items-center justify-center hover:bg-gray-400 w-full lg:w-[190px] md:w-[111px]">
@@ -80,6 +66,45 @@ function Navbar() {
           </Link>
         </div>
       </div>
+
+      {/* search input for mobil */}
+      <div className="mb-2">
+        <form className="w-[99%] mx-auto block md:hidden ">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="ARADIĞINIZ ÜRÜNÜ YAZINIZ."
+              required
+            />
+          </div>
+        </form>
+      </div>
+
 
       <MenuNavbarWeb />
 
